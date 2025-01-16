1427ae5fa8d92a1afbe77c1df2753367d42390bc
David <david11jv@gmail.com>
2025-01-16 16:51:07 +0100

    add send email feature

--- utilities/news_summarize.py
+++ utilities/news_summarize.py
@@ -1,5 +1,8 @@
+import os
 import re
+import smtplib
 from collections import Counter
+from email.utils import formatdate
 
 import requests
 from bs4 import BeautifulSoup
@@ -11,43 +14,49 @@ def fetch_latest_news(article_position):
     response = requests.get(url)
 
     if response.status_code != 200:
-        raise Exception(f"Error al acceder a la URL. Código: {response.status_code}")
+        raise Exception(f"Error fetching the URL. Code: {response.status_code}")
 
     page = BeautifulSoup(response.text, 'html.parser')
 
     div_recent_list = page.find("div", class_="section-recent-list")
 
     if not div_recent_list:
-        raise Exception("No se encontró ningúna lista de recientes")
+        raise Exception("Doesn't find any recent list")
 
     article = div_recent_list.find_all("article")[article_position]
 
     first_news = article.find("a", href=True)
 
     if not first_news:
-        raise Exception("No se encontró ningún enlace dentro del h2.")
+        raise Exception("Doesn't find any link.")
 
     news_title = first_news.get_text(strip=True)
+
     news_link = first_news['href']
 
     if not news_link.startswith("http"):
         news_link = "https://www.genbeta.com" + news_link
+
     return news_title, news_link
 
 
 def fetch_article(url):
     response = requests.get(url)
+
     if response.status_code != 200:
-        raise Exception(f"Error al obtener el artículo. Código: {response.status_code}")
+        raise Exception(f"Error fetching the article. Code: {response.status_code}")
 
     page = BeautifulSoup(response.text, 'html.parser')
 
     article_body = page.find("div", {"class": "article-content"})
+
     if not article_body:
-        raise Exception("No se pudo encontrar el contenido del artículo.")
+        raise Exception("Is not possible to find content for this artícle.")
 
     paragraphs = article_body.find_all('p')
+
     content = " ".join([p.get_text(strip=True) for p in paragraphs])
+
     return content
 
 
@@ -78,26 +87,52 @@ def summarize_article(text, sentences_count=3):
     return " ".join(ordered_summary)
 
 
+def send_email_with_summaries(message_body):
+    email_from = os.getenv("EMAIL_FROM")
+    email_to = os.getenv("EMAIL_TO")
+    password = os.getenv("EMAIL_PASSWORD")
+
+    subject = f"News summary: {formatdate(localtime=True)}"
+
+    message = f"Subject: {subject}\n\n{message_body}"
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()
+        server.login(email_from, password)
+        result = server.sendmail(email_from, email_to, message)
+        server.quit()
+
+        if result:
+            raise Exception("The email fail to send it")
+
+
 def main():
     try:
-
+        summaries = ""
         for i in range(0, 3):
 
             news_title, news_link = fetch_latest_news(i)
-            print(f"Noticia encontrada: {news_title} ({news_link})")
+            summaries += f"New find: {news_title} ({news_link})"
+            print(f"New find: {news_title} ({news_link})")
 
             content = fetch_article(news_link)
 
             if content:
                 summary = summarize_article(content)
-                print("\n=== Resumen ===\n")
+                summaries += "\n=== Resume ===\n"
+                print("\n=== Resume ===\n")
                 lines = summary.split(". ")
                 for line in lines:
+                    summaries += f"- {line.strip()}.\n"
                     print(f"- {line.strip()}.")
+
+                summaries += "\n" + "=" * 20 + "\n"
                 print("\n" + "=" * 20 + "\n")
             else:
-                print("No se pudo obtener contenido para esta noticia.")
+                print("Is not possible to retrieve content for this new.")
 
+        send_email_with_summaries(summaries)
+        print("Success!")
     except Exception as e:
         print(f"Error: {e}")
 
